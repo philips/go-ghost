f995f9b871b469ffc4e36e8a25a2f628acfb1579
Brandon Philips <brandon@ifup.org>
2020-08-16 00:33:34 +0000

    types: collapse all the types to one file

--- types.go
+++ types.go
@@ -4,6 +4,22 @@ import (
 	"time"
 )
 
+type Author struct {
+	ID              *string `json:"id"`
+	Name            *string `json:"name"`
+	Slug            *string `json:"slug"`
+	ProfileImage    *string `json:"profile_image"`
+	CoverImage      *string `json:"cover_image"`
+	Bio             *string `json:"bio"`
+	Website         *string `json:"website"`
+	Location        *string `json:"location"`
+	Facebook        *string `json:"facebook"`
+	Twitter         *string `json:"twitter"`
+	MetaTitle       *string `json:"meta_title"`
+	MetaDescription *string `json:"meta_description"`
+	URL             *string `json:"url"`
+}
+
 type PostRequest struct {
 	Posts  []Post  `json:"posts,omitempty"`
 	Errors []Error `json:"errors,omitempty"`
@@ -37,4 +53,36 @@ type Post struct {
 	PrimaryTag         *Tag       `json:"primary_tag,omitempty"`
 	URL                *string    `json:"url,omitempty"`
 	Excerpt            *string    `json:"excerpt,omitempty"`
+}
+
+type Tag struct {
+	ID              *string   `json:"id"`
+	Name            *string   `json:"name"`
+	Slug            *string   `json:"slug"`
+	Description     *string   `json:"description"`
+	FeaturedImage   *string   `json:"featured_image"`
+	Visibility      *string   `json:"visibility"`
+	MetaTitle       *string   `json:"meta_title"`
+	MetaDescription *string   `json:"meta_description"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	Parent          *Tag      `json:"parent"`
+}
+
+type Error struct {
+	ID      *string `json:"id"`
+	Message *string `json:"message"`
+	Context *string `json:"context"`
+	Type    *string `json:"type"`
+	/*
+		TODO: determine types of these properties
+		Details  *string `json:"details"`
+		Property *string `json:"property"`
+		Help     *string `json:"help"`
+		Code     *string `json:"code"`
+	*/
+}
+
+func (e Error) Error() string {
+	return *e.Message + ": " + *e.Context
 }
